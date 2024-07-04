b3fa9fe98391644c2803659bb82640946289c4e7
Lavender <anyangolavenderr@gmail.com>
2024-07-04 17:33:11 +0300

    Suubi landing page

--- suubi-solutions/src/Landing/index.js
+++ suubi-solutions/src/Landing/index.js
@@ -4,7 +4,7 @@ const Landing = function (){
     return(
         <section className="banner" style={{ backgroundImage: `url(${dashboard})` }}>            
             <div className="content">
-                <h3>SUUBI is what your SACCO needs to move to the next level.<br/>Automate your SACCO now!</h3>
+                <h3 className='body-text'>SUUBI is what your SACCO needs to move to the next level.<br/>Automate your SACCO now!</h3>
                 <button className='get-started-btn'>Get Started</button>
             </div>
         </section>
