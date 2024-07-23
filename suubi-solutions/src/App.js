04f03afc0a34198ac4e6d94d4e628b50548d159d
Brenda15-sys <khamalibreda@gmail.com>
2024-07-23 12:20:31 +0300

    added products

--- suubi-solutions/src/App.js
+++ suubi-solutions/src/App.js
@@ -1,23 +1,13 @@
-import About from "./About";
-import React from 'react';
-import Navbar from './Navbar';
-import Landing from './Landing';
-import Footer from './Footer/index';
-import './App.css';
-import Team from './Ourteam';
-import Product from "./Product"
+import React from "react";
 
-  function App() {
-    return (
-      <div>
-        <Navbar />
-        <Landing/>
-        <Product/>
-        <About/>
-        <Team/>
-        <Footer/>
-      </div>
-    );
-  }
+import Product from "./Product";
+
+function App() {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
 
 export default App;
