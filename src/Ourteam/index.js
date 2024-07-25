1b4e113e40e2dec053b6e8a3e21ebc0aba472b3c
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-07-25 13:13:17 +0300

    made changes

--- src/Ourteam/index.js
+++ src/Ourteam/index.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import Brenda  from "../images/Brenda.jpg";
+import Brenda from "../images/Brenda.jpg";
 import Lavender from "../images/Lavender.jpg";
 import Valentine from "../images/Valentine.jpg";
 import Nakato from "../images/Nakato.jpg";
@@ -7,61 +7,42 @@ import Faith from "../images/Faith.jpg";
 
 const Team = () => {
   return (
-    
-      <div className="meetteam">
+    <div className="team">
+      <div>
+      <h2 className="ourteam">Meet The Team</h2>
+      </div>
 
-        <div>
-        <h2 className="ourteam">Meet The Team</h2>
-        </div>
-
-        
-          
-          
-          <div className="team">
-          <div className="nakato">
+      <div className="members">
+        <div className="nakato">
           <img src={Nakato} alt="Nakato" className="teamimages" />
-          
-          <div className="membername">
-            <h4>Nakato Musana</h4>
-            {/* <p>Data Analyst</p> */}
-          </div>
-          </div>
 
-          <div className="valentine">
-          <img src={Valentine} alt="Valentine" className="teamimages" />
-          <div className="membername">
-            <h4>Valentine Nkatha</h4>
-           
-            </div>
-          </div>
-          <div className="lavender">
-            <img src={Lavender} alt="Lavender" className="teamimages" />
-            <div className="membername">
-              <h4>Lavender Anyango</h4>
-              
-            </div>
-           </div> 
-           
-           
-          <div className="brenda">
-          <img src={Brenda} alt="Brenda" className="teamimages" />
-          <div className="membername">
-            <h4>Brenda Khamali</h4>
-          
-            </div>
-          </div>
-
-          <div className="faith">
-          <img src={Faith} alt="Faith" className="teamimages" />
-          <div className="membername">
-            <h4>Faith Wanyoike</h4>
-            
-          </div>
-          </div>
-        
+          <h4>Nakato Musana</h4>
         </div>
-       </div>
-    
+
+        <div className="valentine">
+          <img src={Valentine} alt="Valentine" className="teamimages" />
+
+          <h4>Valentine Nkatha</h4>
+        </div>
+        <div className="lavender">
+          <img src={Lavender} alt="Lavender" className="teamimages" />
+
+          <h4>Lavender Anyango</h4>
+        </div>
+
+        <div className="brenda">
+          <img src={Brenda} alt="Brenda" className="teamimages" />
+
+          <h4>Brenda Khamali</h4>
+        </div>
+
+        <div className="faith">
+          <img src={Faith} alt="Faith" className="teamimages" />
+
+          <h4>Faith Wanyoike</h4>
+        </div>
+      </div>
+    </div>
   );
 };
 export default Team;
